73a614f13f16c824be03e1fccaff13e0e65259d4
torco34 <torcoromaarias@gmail.com>
2025-08-11 12:42:04 -0500

    route react install

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
-import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
+import EducationPage from "./pages/EducationPage";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import SkillsPage from "./pages/SkillsPage";
 
+import "./App.css";
 export default function App() {
   return (
-    <>
-      <h2 className="text-sky-500">hola mundo ////</h2>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/education" element={<EducationPage />} />
+        <Route path="/skills" element={<SkillsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 }
