a371faf0261347cb25ad2d8a2252772835b27343
torco34 <torcoromaarias@gmail.com>
2025-08-14 14:50:13 -0500

    conectado bien basedata

--- backend/src/DB/mysql.js
+++ backend/src/DB/mysql.js
@@ -1,31 +1,47 @@
 const mysql = require("mysql2");
 const config = require("../config/config.js");
 let connection;
-const dbconfig = {
+const dbConfig = {
     host: config.mysql.host,
     user: config.mysql.user,
     password: config.mysql.password,
     database: config.mysql.database,
 }
-function connect() {
-    connection = mysql.createConnection(dbconfig);
+function createMysql() {
+    connection = mysql.createConnection(dbConfig);
     connection.connect((err) => {
         if (err) {
-            console.error("Error connecting to the database:", err);
-            return;
+            console.log('[Error connecting to the database:]', err);
+            setTimeout(createMysql, 2000);
+        } else {
+            console.log('Connected to the MySQL database');
         }
-        console.log("Connected to the MySQL database");
+    });
+
+    connection.on('error', (err) => {
+        console.log('[Database error:]', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            createMysql();
+        } else {
+            throw err;
+        }
     });
 }
 
-function getConnection() {
-    if (!connection) {
-        connect();
-    }
-    return connection;
+createMysql();
+
+function all(tabla) {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT * FROM ${tabla}`, (err, results) => {
+            if (err) {
+                console.error('Error fetching data:', err);
+                return reject(err);
+            }
+            resolve(results);
+        });
+    });
 }
 
 module.exports = {
-    connect,
-    getConnection,
+    all,
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
+// src/index.js
+require('dotenv').config();
 const app = require('./app');
 
 app.listen(app.get('port'), () => {
-    console.log(`Server running on port ${app.get('port')}`);
+    console.log(`Servidor corriendo en el puerto ${app.get('port')} 🚀`);
 });
