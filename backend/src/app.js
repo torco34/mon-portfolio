ebb3c16acff7c371f21596e4f5597c4a6de70c61
torco34 <torcoromaarias@gmail.com>
2025-09-17 13:41:58 -0500

    orden

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,8 +27,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
+
 // Habilitar CORS
-app.use(cors({ origin: "http://localhost:5174" })); // o "*" en desarrollo
+app.use(cors({ origin: "http://localhost:5173" })); // o "*" en desarrollo
 
 // Puerto
 app.set('port', config.app.port || 4000);
